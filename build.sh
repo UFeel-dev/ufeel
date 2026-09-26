77913438af307d5f3f20857e56980d888282d8df
Manish Jangir <121192543+ManishjangirIITG@users.noreply.github.com>
2026-09-26 05:31:14 +0000

    back to j8 from j2 for production

--- build.sh
+++ build.sh
@@ -22,7 +22,7 @@ compile()
 {
     echo "Compiling..."
     cd $BUILD_DIR
-    make -j2    #just for the devlopment phase, will change when creating a pr
+    make -j8    #just for the devlopment phase, will change when creating a pr
     cd ..
 }
 
